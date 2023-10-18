3e827b46c31d0732fc45bd6b74593d06e948f726
Billy Laws <blaws05@gmail.com>
2023-10-18 21:15:15 +0100

    TEMP: use arm64ec specific compiler-rt

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -636,6 +636,9 @@ static StringRef getArchNameForCompilerRTLib(const ToolChain &TC,
   if (TC.getArch() == llvm::Triple::x86_64 && Triple.isX32())
     return "x32";
 
+  if (Triple.isWindowsArm64EC())
+    return "arm64ec";
+
   return llvm::Triple::getArchTypeName(TC.getArch());
 }
 
