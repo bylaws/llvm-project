832f53e7ca9af08044d8a205efbfb91ddd96e317
Billy Laws <blaws05@gmail.com>
2024-01-28 23:45:12 +0000

    [compiler-rt] Mangle ARM64EC assembly function symbol names
    ARM64EC requires that native function symbols be prefixed with a '#'
    when defined in assembly, however __USER_LABEL_PREFIX__ isn't defined as
    that is generally used to prefix both data and function symbols.

--- compiler-rt/lib/builtins/assembly.h
+++ compiler-rt/lib/builtins/assembly.h
@@ -207,7 +207,13 @@
 #define GLUE4_(a, b, c, d) a##b##c##d
 #define GLUE4(a, b, c, d) GLUE4_(a, b, c, d)
 
+#ifdef __arm64ec__
+#define QUOTE(a) #a
+#define STR(a) QUOTE(a)
+#define SYMBOL_NAME(name) STR(GLUE(#, name))
+#else
 #define SYMBOL_NAME(name) GLUE(__USER_LABEL_PREFIX__, name)
+#endif
 
 #ifdef VISIBILITY_HIDDEN
 #define DECLARE_SYMBOL_VISIBILITY(name)                                        \
