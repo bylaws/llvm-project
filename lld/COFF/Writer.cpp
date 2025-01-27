06b111494a8d2d3326c32aac5e76c5c8fa0fca2f
Jacek Caban <jacek@codeweavers.com>
2025-01-28 00:04:07 +0100

    WIP: local imports

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -1192,10 +1192,12 @@ void Writer::createMiscChunks() {
   }
 
   // Create thunks for locally-dllimported symbols.
-  if (!ctx.symtab.localImportChunks.empty()) {
-    for (Chunk *c : ctx.symtab.localImportChunks)
-      rdataSec->addChunk(c);
-  }
+  ctx.forEachSymtab([&](SymbolTable &symtab) {
+    if (!symtab.localImportChunks.empty()) {
+      for (Chunk *c : symtab.localImportChunks)
+        rdataSec->addChunk(c);
+    }
+  });
 
   // Create Debug Information Chunks
   debugInfoSec = config->mingw ? buildidSec : rdataSec;
